490e5481e97d7f9decf64b05ec47eaa5077cc286
unknown <xqtwkk@gmail.com>
2023-05-15 17:40:36 +0100

    cat done

--- src/lt/kvk/i4_2/olga_korolkova/main/animals/Cat.java
+++ src/lt/kvk/i4_2/olga_korolkova/main/animals/Cat.java
@@ -1,5 +1,14 @@
 package lt.kvk.i4_2.olga_korolkova.main.animals;
 
-public class Cat {
-    
+import lt.kvk.i4_2.olga_korolkova.main.Animal;
+
+public class Cat extends Animal {
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
 }
