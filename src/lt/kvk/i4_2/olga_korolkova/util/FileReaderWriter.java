3f3036b3581a33c6fdfb491c22f2c8e567b98dd6
unknown <xqtwkk@gmail.com>
2023-05-15 18:01:44 +0100

    file read, write

--- src/lt/kvk/i4_2/olga_korolkova/util/FileReaderWriter.java
+++ src/lt/kvk/i4_2/olga_korolkova/util/FileReaderWriter.java
@@ -1,5 +1,24 @@
 package lt.kvk.i4_2.olga_korolkova.util;
 
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class FileReaderWriter {
-    
+    public static List<String> readLinesFromFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            throw new FileNotFoundException("File not found: " + filePath);
+        }
+        return Files.readAllLines(Path.of(filePath));
+    }
+
+    public static void writeLinesToFile(String filePath, List<String> lines) throws IOException {
+        Files.write(Path.of(filePath), lines, StandardOpenOption.APPEND);
+    }
 }
+
