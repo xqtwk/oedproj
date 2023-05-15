be0c2e96b6aef3d00bb851ddcf4c11730dde9e07
unknown <xqtwkk@gmail.com>
2023-05-15 17:39:45 +0100

    add data to data files, animal class implemented(via soundable interface)

--- src/lt/kvk/i4_2/olga_korolkova/main/Animal.java
+++ src/lt/kvk/i4_2/olga_korolkova/main/Animal.java
@@ -1,5 +1,25 @@
 package lt.kvk.i4_2.olga_korolkova.main;
 
-public class Animal {
-    
+public abstract class Animal implements Soundable {
+    private String name;
+    private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        }
+    }
 }
