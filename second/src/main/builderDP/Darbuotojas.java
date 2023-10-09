5f929656a7d275ddc319f7d5f7421f42b03b68d8
xqtwk <xqtwkk@gmail.com>
2023-10-10 00:47:52 +0100

    moved

--- second/src/main/builderDP/Darbuotojas.java
+++ second/src/main/builderDP/Darbuotojas.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Darbuotojas {
     private String vardas;
     private String pavarde;
--- second/src/main/singletonDP/Direktorius.java
+++ second/src/main/singletonDP/Direktorius.java
@@ -1,4 +1,4 @@
-package main;
+package main.singletonDP;
 
 public class Direktorius {
     private static Direktorius direktoriusInstance;
